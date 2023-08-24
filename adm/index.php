f071b298512dd350cfddd054323e8686cab2020c
TatianaAlves1 <tatipublisher@gmail.com>
2023-08-24 16:55:11 -0400

    arquivos para gerenciamento do sistema

---
+++ adm/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <title>Dashboard</title>
+    <style>
+      .topo-img{
+        height:180px;
+        display:block;
+        margin: 0 auto;
+      }
+      
+    </style>
+</head>
+<body>
+<header class="container pt-4">
+  <img src="../img/ECOS_logo oficial_1.png" alt="logo ecos" class="img-fluid topo-img">
+</header>
+    <main>
+        <section class="container border text-center mt-5 d-flex justify-content-between align-items-center p-5  ">
+            <div class="row">
+                <div class="col"> 
+                <a href="#">Lista Geral</a> 
+                </div>
+                <div class="col">
+                    <a href="#">Pesquisa</a>
+                </div>
+                <div class="col">
+                    <a href="#">Cadastrar ADM</a>
+                </div>
+            </div>
+            
+        </section>
+    </main>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+</body>
+</html>
